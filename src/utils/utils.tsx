86b069a9e38fdcbd9cd72af2a367d821dcffb2b5
zack5 <zack5@users.noreply.github.com>
2025-03-07 09:33:45 -0500

    Added underline state to active navlink style so it's not just distinguished by color

--- src/utils/utils.tsx
+++ src/utils/utils.tsx
@@ -1,3 +1,3 @@
 export function getActiveNavLinkStyles({ isActive }: { isActive: boolean }) {
-  return isActive ? { color: 'var(--color-accent)' } : {}
+  return isActive ? { color: 'var(--color-accent)', textDecoration: 'underline' } : {}
 }
